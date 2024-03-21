25a49664fb663ec02285789e933b631a3a6a5e25
Fabian Jaeger <fabian_jaeger@outlook.com>
2024-03-21 18:42:25 +0100

    sidebar-button

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,13 +10,15 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatStepperModule } from "@angular/material/stepper";
 import { NavbarComponent } from './navbar/navbar.component';
 import { HoverButtonComponent} from './hover-button/hover-button.component';
+import { SidebarComponent } from './navbar/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ModalWrapperComponent,
     NavbarComponent,
-    HoverButtonComponent
+    HoverButtonComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
