7d6c554f757ea85aa0339f198114fbd0e1b75e1b
Mateusz Halada <haladamateusz@gmail.com>
2024-04-28 17:39:58 +0200

    filter companies with lookup status FOUND

--- backend/flaskr/extract_companyLongName_and_ISIN_BC.py
+++ backend/flaskr/extract_companyLongName_and_ISIN_BC.py
@@ -1,6 +1,8 @@
 import json
 import pandas as pd
 from six_api_requests import FinancialDataAPI
+from pydash import get
+import urllib.error
 
 df = pd.read_csv('../input-data/EUESGMANUFACTURER.csv')
 
@@ -18,15 +20,37 @@ findata = FinancialDataAPI('/Users/mateuszhalada/Hackaton_St_Gallen/eco-sixtem/b
 
 def sendIntradayRequest(companyLongName: str, ISIN_BC: str):
     time.sleep(1)
-    print('sleeping with ', companyLongName)
-    return {
-      'status': findata.intradaySnapshot("ISIN_BC", [ISIN_BC]),
-      'companyLongName': companyLongName
-    }
+    print('sleeping with ', companyLongName, ISIN_BC)
+    try:
+        intradaySnapshot = findata.intradaySnapshot("ISIN_BC", [ISIN_BC])
+    except:
+        return 'ERROR'
+    else:
+        return get(intradaySnapshot, 'data.listings')[0]['lookupStatus']
 
 
-result = [sendIntradayRequest(cln, isin_bc) for cln, isin_bc in zip(data['companyLongName'], data['ISIN_BC']) ]
-print(result)
+#result = [sendIntradayRequest(cln, isin_bc) for cln, isin_bc in zip(data['companyLongName'], data['ISIN_BC']) ]
+
+data.reset_index()
+
+#val = sendIntradayRequest(data['companyLongName'].iloc[0], data['ISIN_BC'].iloc[0])
+
+#for index, row in data.iterrows():
+#    val = sendIntradayRequest(row['companyLongName'], row['ISIN_BC'])
+#    data.loc[df.index[index], 'lookupStatus'] = val
+#    outfile2 = open('lookup_status.csv', 'wb')
+#    data.to_csv(outfile2, index=False, header=True, sep=',', encoding='utf-8')
+#    outfile2.close()
+
+
+dataWithLookupStatus = pd.read_csv('./lookup_status.csv')
+data2 = dataWithLookupStatus[dataWithLookupStatus['lookupStatus'] == 'FOUND']
+
+outfile3 = open('lookup_status_found.csv', 'wb')
+data2.to_csv(outfile3, index=False, header=True, sep=',', encoding='utf-8')
+outfile3.close()
+
+
 
 # sample1 = findata.intradaySnapshot("ISIN_BC", [first_row['ISIN_BC']])
 
